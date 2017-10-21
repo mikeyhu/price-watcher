eed6ad747d12d7a655b21df64d02660a560caec8
Michael Hughes <mikee.hughes@gmail.com>
2017-10-21 21:40:13 +0100

    watcher can record price with timestamp

--- src/price_watcher/watcher.clj
+++ src/price_watcher/watcher.clj
@@ -1,7 +1,12 @@
 (ns price-watcher.watcher
   (:require [clj-time.core :as t]
-            [clj-time.coerce :as tc]))
+            [clj-time.coerce :as tc]
+            [price-watcher.history :as history]))
 
 (defn timestamp [] (tc/to-long (t/now)))
 
-(defn with-timestamp [data] [(timestamp) data])
+(defn with-timestamp [data] {(timestamp) data})
+
+(defn add-price-to-history
+  [store price]
+  (history/with store (with-timestamp price)))
--- test/price_watcher/watcher_test.clj
+++ test/price_watcher/watcher_test.clj
@@ -1,16 +1,25 @@
 (ns price-watcher.watcher-test
   (:require [clojure.test :refer :all]
             [clj-time.core :as t]
-            [price-watcher.watcher :as watcher]))
+            [price-watcher.watcher :as w]
+            [price-watcher.history :as h]))
 
 (deftest create-timestamp
   (with-redefs [t/now (fn [] (t/date-time 2017 10 21))]
-    (is (= 1508544000000 (watcher/timestamp)))
+    (is (= 1508544000000 (w/timestamp)))
     ))
 
 (deftest key-by-timestamp
   (with-redefs [t/now (fn [] (t/date-time 2017 10 21))]
-    (let [kv (watcher/with-timestamp 7)]
-      (is (= 1508544000000 (nth kv 0)))
-      (is (= 7 (nth kv 1)))
+    (let [kv (w/with-timestamp 7)]
+      (is (= 7 (get kv 1508544000000)))
       )))
+
+(deftest adds-price-to-history
+  (with-redefs [t/now (fn [] (t/date-time 2017 10 21))]
+    (let [store (h/init)
+          value 7
+          updated-store (w/add-price-to-history store value)]
+      (is (= 7 (val (h/most-recent updated-store))))
+      )))
+
