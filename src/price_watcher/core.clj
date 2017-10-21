e4d3214b0355ddb1a4721e4f09fc26c587e59b34
Michael Hughes <mikee.hughes@gmail.com>
2017-10-21 22:10:10 +0100

    Retrieve and save the current price on each run

--- src/price_watcher/core.clj
+++ src/price_watcher/core.clj
@@ -1,8 +1,17 @@
 (ns price-watcher.core
   (:gen-class)
-  (:require [price-watcher.collector :as collector]))
+  (:require [price-watcher.collector :as collector]
+            [price-watcher.history :as history]
+            [price-watcher.watcher :as watcher]))
+
+(def location (str (System/getProperty "user.dir") "/.price-watcher.cljdata"))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println (collector/collect-price :ETH :GBP)))
+  (let
+    [current-data (history/load-from location)
+     latest-price (collector/collect-price :ETH :GBP)]
+    (history/save-to
+      location
+      (watcher/add-price-to-history current-data latest-price)
+      )))
